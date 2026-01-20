36c78c0570487e2c9bcca8b15c2b0006b5af5719
Jorge Moreno <jmoreno@gatech.edu>
2026-01-19 19:00:43 -0800

    Complete etch-a-sketch

--- javascript.js
+++ javascript.js
@@ -1,10 +1,46 @@
 const container = document.querySelector('#container');
+const resizeBtn = document.querySelector('#resize-btn')
 
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
+const containerSize = 800; // Total widht and height in pixels
 
-for (let i = 0; i < totalSquares; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function createGrid(gridSize) {
+
+    // Clear existing grid
+    container.innerHTML = '';
+
+    const SquareSize = containerSize / gridSize;
+    const totalSquares = gridSize * gridSize;
+
+
+    for (let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+
+        square.style.width = `${SquareSize}px`;
+        square.style.height = `${SquareSize}px`;
+
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'black';
+        });
+
+        container.appendChild(square);
+    }
 }
+
+// Start Grid with 16x16
+createGrid(16);
+
+// Resize button
+resizeBtn.addEventListener('click', () => {
+    let userInput = prompt('Enger number of squares per size:')
+    if (userInput === null) return;
+    const gridSize = Number(userInput);
+
+    if (gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
+        alert("Please enter an integer number between 1 and 100");
+        return;
+    }
+
+    createGrid(gridSize);
+
+});
